27932f23855b511268b47215bcf4d4da5af2e906
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-03-21 13:14:37 +0200

    Middleman settings was changed - Cache buster was disabled - JS and CSS files were compressed

--- config.rb
+++ config.rb
@@ -55,13 +55,13 @@ set :haml, format: :html5
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  #activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
