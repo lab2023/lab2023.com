93bb52ef9dcc00511f6c75ff4537417f7066bc60
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-03-21 12:59:53 +0200

    Middleman deploy plugin was enabled

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'middleman', '~>3.2.2'
 # Live-reloading plugin
 gem 'middleman-livereload', '~> 3.1.0'
 
+# Deploy plugin
+gem 'middleman-deploy', '~> 0.2.3'
+
 # For faster file watcher updates on Windows:
 gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
 
--- config.rb
+++ config.rb
@@ -69,3 +69,11 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  # Optional Settings
+  # deploy.remote = "custom-remote" # remote name or git url, default: origin
+  # deploy.branch = "custom-branch" # default: gh-pages
+end
